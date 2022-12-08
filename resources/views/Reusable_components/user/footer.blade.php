253b45506e00472839b5708a5d34354d7b8d9bcb
Komal <komalrathore1101@gmail.com>
2022-12-08 12:25:33 +0530

    first commit

---
+++ resources/views/Reusable_components/user/footer.blade.php
@@ -0,0 +1,135 @@
+<!-- Footer -->
+<footer id="footer" class="page-footer font-small  pt-4" style="background:#0a2929;">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">About</h6>
+        <p> Banarsi Lehange House sells exquisitely designed❤️, handcrafted lehengas. When you wear this to the event, you'll look best.😍</p>
+       
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+        <p>
+          <a href="{{url('Shop/Flowers-Fruits-Basket-Set')}}" target="_blank">Sherwani </a>
+        </p>
+        <p>
+          <a href="{{url('Shop/Dusbin')}}" target="_blank">Bridal Lehnga</a>
+        </p>
+        <p>
+          <a href="{{url('Shop/salwar-suit')}}" target="_blank">Lehnga</a>
+        </p>
+        <p>
+          <a href="{{url('Shop/Containers')}}" target="_blank">Sherwani</a>
+        </p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+        <p>
+          <a href="{{url('about')}}" target="_blank">About Us</a>
+        </p>
+       
+        <p>
+          <a href="{{url('Help')}}" target="_blank">Help</a>
+        </p>
+        <p>
+          <a href="{{url('register')}}" target="_blank">Signup</a>
+        </p>
+          <p>
+          <a href="{{url('login')}}" target="_blank">Login</a>
+        </p>
+      </div>
+
+      <!-- Grid column -->
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+        <p>
+          <i class="fas fa-home mr-3"></i>  SKU, New B.O.: 224, Brand Rath Road, Rainak Bazar, Opp. Naaz Complex, Jalandhar City.</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i>  Aggarwalsparsh@gmail.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> +91 99883 63656</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> +91 78890 76841</p>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Footer links -->
+
+    <hr>
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+
+        <!--Copyright-->
+        <p class="text-center text-md-left">
+        <a href="https://omenterprisesgroup.in" target="_blank" style=" font-family: 'Balsamiq Sans', cursive;"><u>Omenterprisesgroup.in</u></a>
+          <br>
+          <span> Developed by <a href="#"  style=" font-family: 'Balsamiq Sans', cursive;"><u>Komal</u></a> </span>
+        </p>
+       
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-5 col-lg-4 ml-lg-0">
+
+        <!-- Social buttons -->
+        <div class="text-center text-md-right">
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a href="https://www.facebook.com/" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li  class="list-inline-item">
+              <a href="https://twitter.com/" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.instagram.com/" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="https://www.linkedin.com/in/" class="btn-floating btn-sm rgba-white-slight mx-1" target="_blank">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
